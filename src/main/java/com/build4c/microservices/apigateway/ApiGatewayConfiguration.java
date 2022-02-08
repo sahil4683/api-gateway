44e6af7b3814ac69bebf577ef20e7d8d068cbabb
Sahil Patil <43109674+sahil4683@users.noreply.github.com>
2022-02-08 18:55:17 +0530

    Initial commit

---
+++ src/main/java/com/build4c/microservices/apigateway/ApiGatewayConfiguration.java
@@ -0,0 +1,37 @@
+package com.build4c.microservices.apigateway;
+
+import java.util.function.Function;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+	@Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builter) {
+		return builter.routes()
+				.route(p -> p.path("/get")
+				.filters(f-> f.addRequestHeader("MyHeader", "MyUrl")
+						.addRequestParameter("MyParam", "MyValue"))
+				.uri("http://httpbin.org:80"))
+				
+				.route(p-> p.path("/currency-exchange/**").uri("lb://currency-exchange"))
+				.route(p-> p.path("/currency-conversion/**").uri("lb://currency-conversion"))
+				
+				.route(p-> p.path("/ce/**")
+						.filters(f-> f.rewritePath("ce", "currency-exchange"))
+						.uri("lb://currency-exchange"))
+				.route(p-> p.path("/cc/**")
+						.filters(f-> f.rewritePath("cc/(?<segment>.*)", "currency-conversion/${segment}"))
+						.uri("lb://currency-conversion"))
+				
+				.build();
+	}
+	
+}
